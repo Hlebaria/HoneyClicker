e027205bbc3d85a6663cf100f60a5d535dc35625
Hlebaria <100922367+Hlebaria@users.noreply.github.com>
2024-06-08 14:46:35 +0300

    Manual Update Main.java

--- HoneyClicker/src/main/java/org/example/Main.java
+++ HoneyClicker/src/main/java/org/example/Main.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import org.example.model.Game;
+import org.example.model.GlobalClock;
+import org.example.model.RunnableTimer;
 
 import java.io.IOException;
 
@@ -8,6 +10,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         Game game = Game.getInstance();
+
+        GlobalClock clock = GlobalClock.getInstance();
+        Thread runnableTimer = new Thread(new RunnableTimer(clock));
+        runnableTimer.start();
 
     }
 }
