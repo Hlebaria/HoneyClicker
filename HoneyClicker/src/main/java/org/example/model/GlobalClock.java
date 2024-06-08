9a3e69b3b3f8b5b84b72c8f7c9f0a55bd87690f3
Hlebaria <100922367+Hlebaria@users.noreply.github.com>
2024-06-08 14:50:03 +0300

    Manual Update GlobalClock.java

--- HoneyClicker/src/main/java/org/example/model/GlobalClock.java
+++ HoneyClicker/src/main/java/org/example/model/GlobalClock.java
@@ -3,15 +3,28 @@ package org.example.model;
 public class GlobalClock {
 
     private static GlobalClock instance = new GlobalClock();
+    private static int hours = 0;
 
     public static GlobalClock getInstance() {
         return instance;
     }
 
     public void addHour(){
+        hours++;
+        if(hours >= 24){
+            hours = 0;
+        }
+
+        Game.advanceTime();
 
     }
 
-    //направи часовник, който има само часове
+    public static boolean daytime(){
+        return hours > 6 & hours <= 18;
+    }
+
+    public int getHours(){
+        return hours;
+    }
 
 }
