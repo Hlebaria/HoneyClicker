b35ceeb2cb4173abec68dc760f0935d78dc321e0
Hlebaria <100922367+Hlebaria@users.noreply.github.com>
2024-06-08 14:47:11 +0300

    Manual Update Bank.java

--- HoneyClicker/src/main/java/org/example/model/Bank.java
+++ HoneyClicker/src/main/java/org/example/model/Bank.java
@@ -5,7 +5,11 @@ public class Bank extends Item{
     public Bank(){
         super();
         description = "HONEY BANK\n\nUses the global economy to generate honey interest and gain honey.\nA bank makes 10,000 droplets of honey each second.\nWorks day and night.";
-        //other params
+        honeyPerTick = 10000;
+        timeInterval = 1000; //1 sec
+        dayShift = true;
+        nightShift = false;
+        price = 100000;
     }
 
 }
