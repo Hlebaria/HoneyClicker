5951c4ef8f94263cab2844cf1dd989d9fd4ddf89
Hlebaria <100922367+Hlebaria@users.noreply.github.com>
2024-06-08 14:47:44 +0300

    Manual Update Bee.java

--- HoneyClicker/src/main/java/org/example/model/Bee.java
+++ HoneyClicker/src/main/java/org/example/model/Bee.java
@@ -5,7 +5,11 @@ public class Bee extends Item{
     public Bee(){
         super();
         description = "BEE\n\nAn honest worker that makes honey.\nA bee makes 1 droplet of honey each second.\nWorks during the day.";
-        //other params
+        honeyPerTick = 1;
+        timeInterval = 1000; //1 sec
+        dayShift = true;
+        nightShift = false;
+        price = 30;
     }
 
 }
