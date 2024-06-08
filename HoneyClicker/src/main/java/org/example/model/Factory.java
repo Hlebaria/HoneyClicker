bad25e2a2615216800fcab78b84c439f5669e05a
Hlebaria <100922367+Hlebaria@users.noreply.github.com>
2024-06-08 14:48:39 +0300

    Manual Update Factory.java

--- HoneyClicker/src/main/java/org/example/model/Factory.java
+++ HoneyClicker/src/main/java/org/example/model/Factory.java
@@ -5,6 +5,10 @@ public class Factory extends Item{
     public Factory(){
         super();
         description = "HONEY FACTORY\n\nIndustrial grade facility for making honey.\nA factory makes 500 droplets of honey each second.\nWorks during the day.";
-        //other params
+        honeyPerTick = 500;
+        timeInterval = 1000; //1 sec
+        dayShift = true;
+        nightShift = false;
+        price = 10000;
     }
 }
