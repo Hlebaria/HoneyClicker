02ae9be1867188ca10c614ea23518768c6e05c36
Hlebaria <100922367+Hlebaria@users.noreply.github.com>
2024-06-08 14:49:00 +0300

    Manual Update Farm.java

--- HoneyClicker/src/main/java/org/example/model/Farm.java
+++ HoneyClicker/src/main/java/org/example/model/Farm.java
@@ -5,7 +5,11 @@ public class Farm extends Item{
     public Farm(){
         super();
         description = "HONEY FARM\n\nA special farm full of beehives that generate honey.\nA farm makes 100 droplets of honey each two seconds.\nWorks day and night.";
-        //other params
+        honeyPerTick = 100;
+        timeInterval = 2000; //2 sec
+        dayShift = true;
+        nightShift = true;
+        price = 1000;
     }
 
 }
